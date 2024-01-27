e0bb6e30d55d73cb027e8cfc8489983d3bc6e709
Namit Nayan <namitnayan32@gmail.com>
2024-01-27 19:33:35 +0530

    OBR-1: Multi Threaded: Use seek.

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,84 @@
 use std::{
-    collections, env, fmt, fs,
-    io::{self, BufRead},
-    ops,
+    collections, env, fmt, fs, io::{self, BufRead, Seek}, ops, sync, thread
 };
 
 fn main() {
+    let num_threads = thread::available_parallelism().unwrap().get() as u64;
     let file_path = match env::args().nth(1) {
         Some(f) => f,
         None => "data/measurements.txt".to_owned(),
     };
     let file = fs::File::open(file_path.clone())
-        .unwrap_or_else(|_| panic!("Measurement file not found in path {:?}", file_path));
-    let mut reader = io::BufReader::new(file);
-    let mut stations_pos = collections::HashMap::<String, usize>::with_capacity(10_000);
-    let mut measures: Vec<Record> = Vec::with_capacity(10_000);
-    let mut line_buff = String::with_capacity(110);
-    while reader.read_line(&mut line_buff).unwrap() > 0 {
-        let (name, measure) = line_buff.split_once(';').unwrap();
-        let measure: f32 = measure.trim().parse().unwrap();
-        if let Some(&id) = stations_pos.get(name) {
-            measures[id] += Record::new(id, measure);
-        } else {
-            stations_pos.insert(name.to_owned(), measures.len());
-            measures.push(Record::new(measures.len(), measure));
-        }
-        line_buff.clear();
+            .expect("Measurement file not found in path.");
+    let file_size = file.metadata().unwrap().len();
+    let chunk_size = file_size / num_threads;
+    let mut stations_measures = collections::HashMap::<String, Record>::with_capacity(10_000);
+    let mut handles = vec![];
+    let (tx, rx) = sync::mpsc::channel();
+    let mut start = 0;
+    let mut buf = String::with_capacity(200);
+    while start < file_size {
+        let file = fs::File::open(file_path.clone())
+            .expect("Measurement file not found in path.");
+        let mut reader = io::BufReader::new(file);
+        let mut end = file_size.min(start + chunk_size);
+        let _ = reader.seek(io::SeekFrom::Start(end));
+        let _ = reader.read_line(&mut buf);
+        end = reader.stream_position().unwrap();
+        buf.clear();
+        let tx1 = tx.clone();
+        let handle = thread::spawn(move || {
+            let chunk_res = process_chunk(reader, start, end);
+            tx1.send(chunk_res)
+        });
+        handles.push(handle);
+        start = end + 1;
     }
-    let mut id_name = vec![""; measures.len()];
-    stations_pos.iter().for_each(|(s, i)| id_name[*i] = s);
-    measures.sort_unstable_by_key(|r| id_name[r.id]);
-    print!("{{");
-    for (i, r) in measures.iter().enumerate() {
-        if i > 0 {
-            print!(", ");
-        }
-        print!("{}={r}", id_name[r.id]);
+    for handle in handles {
+        let _ = handle.join().unwrap();
     }
-    print!("}}");
+    drop(tx);
+    for chunk_res in rx {
+        combine_res(&mut stations_measures, chunk_res)
+    }
+    let output_vec = output(&stations_measures);
+    print!("{{{}}}", output_vec.join(", "));
 }
 
+fn combine_res(acc: &mut collections::HashMap<String, Record>, curr: collections::HashMap<String, Record>) {
+    for (name, record) in curr {
+        acc.entry(name).and_modify(|rec| *rec += record).or_insert(record);
+    }
+}
+
+fn process_chunk(mut reader: io::BufReader<fs::File>, start: u64, end: u64) -> collections::HashMap<String, Record> {
+    // Guranteed that start - 1 is '\n' and end is '\n'
+    let mut stations_measures = collections::HashMap::<String, Record>::with_capacity(10_000);
+    let _ = reader.seek(io::SeekFrom::Start(start));
+    let mut buf = String::with_capacity(120);
+    while reader.stream_position().unwrap() < end {
+        let _ = reader.read_line(&mut buf);
+        let (name, measure) = buf.split_once(';').unwrap();
+        let measure: f32 = measure.trim().parse().unwrap();
+        let record = Record::new(measure);
+        stations_measures.entry(name.to_owned()).and_modify(|rec| *rec += record).or_insert(record);
+        buf.clear();
+    }
+    stations_measures
+}
+
+fn output(stations_measures: &collections::HashMap<String, Record>) -> Vec<String> {
+    let mut keys: Vec<_> = stations_measures.keys().collect();
+    keys.sort_unstable();
+    let mut output_vec = Vec::with_capacity(keys.len());
+    for  name in keys {
+        output_vec.push(format!("{}={}", name, stations_measures.get(name).unwrap()));
+    }
+    output_vec
+}
+
+#[derive(Clone, Copy)]
 struct Record {
-    id: usize,
     low: f32,
     high: f32,
     sum: f64,
@@ -56,7 +94,6 @@ impl fmt::Display for Record {
 impl ops::AddAssign for Record {
     fn add_assign(&mut self, rhs: Self) {
         *self = Self {
-            id: self.id,
             low: self.low.min(rhs.low),
             high: self.high.max(rhs.high),
             sum: self.sum + rhs.sum,
@@ -66,9 +103,8 @@ impl ops::AddAssign for Record {
 }
 
 impl Record {
-    fn new(id: usize, val: f32) -> Self {
+    fn new(val: f32) -> Self {
         Self {
-            id,
             low: val,
             high: val,
             sum: val as f64,
