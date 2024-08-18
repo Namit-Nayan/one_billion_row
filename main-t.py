88954adbe5be95c9b73ea5ed719cefb599822bef
NamitNayan <namitnayan32@gmail.com>
2024-08-19 00:08:13 +0530

    Remove dataclass, python is lot slower.

--- main-t.py
+++ main-t.py
@@ -1,37 +1,12 @@
 import math
 import mmap
-import dataclasses
-from typing import Self
 import concurrent.futures
-num_threads = 12
-@dataclasses.dataclass
-class Record:
-    low: int
-    high: int
-    total: int
-    count: int = 1
+import os
+num_threads: int = os.process_cpu_count()
 
-    def __init__(self, val: int):
-        self.low = val
-        self.high = val
-        self.total = val
-
-    def __iadd__(self, other: Self):
-        self.low = min(self.low, other.low)
-        self.high = max(self.high, other.high)
-        self.total += other.total
-        self.count += other.count
-        return self
-
-    def _mean(self) -> float:
-        mean = self.total / self.count
-        return math.ceil(mean) / 10
-
-    def __str__(self) -> str:
-        return f"{self.low / 10:.1f}/{self._mean():.1f}/{self.high / 10:.1f}"
 
 def main():
-    file_path = "data/measurements.txt"
+    file_path = "data/measurements_medium.txt"
     with open(file=file_path, mode="rb") as file_obj:
         with mmap.mmap(fileno=file_obj.fileno(), length=0, access=mmap.ACCESS_READ) as mmap_obj:
             start_end_pos = get_start_and_end_pos(mmap_obj=mmap_obj)
@@ -40,7 +15,7 @@ def main():
 
 
 def get_output(mmap_obj: mmap.mmap, start_end_pos: list[list[int]]):
-    station_measures: dict[bytes, Record] = {}
+    station_measures: dict[bytes, list[int]] = {}
     futures = []
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as exec:
         for args in start_end_pos:
@@ -51,50 +26,58 @@ def get_output(mmap_obj: mmap.mmap, start_end_pos: list[list[int]]):
     return f"{{{', '.join(output)}}}"
 
 
-def output_list(station_measures: dict[bytes, Record]) -> list[str]:
+def output_list(station_measures: dict[bytes, list[int]]) -> list[str]:
     stations = sorted(station_measures.keys())
     output = []
     for station in stations:
-        output.append(f"{station.decode()}={str(station_measures[station])}")
+        record = station_measures[station]
+        record_mean = math.ceil(record[2] / record[3]) / 10
+        output.append(f"{station.decode()}={record[0] / 10:.1f}/{record_mean:.1f}/{record[1] / 10:.1f}")
     return output
 
 
-def process_chunk(mmap_obj: mmap.mmap, start: int, end: int) -> dict[bytes, Record]:
-    station_measures: dict[bytes, Record] = {}
+def combine_record(record1: list[int], record2: list[int]):
+    # low, high, total, count
+    if record1[0] > record2[0]:
+        record1[0] = record2[0]
+    if record1[1] < record2[1]:
+        record1[1] = record2[1]
+    record1[2] += record2[2]
+    record1[3] += record2[3]
+
+
+def process_chunk(mmap_obj: mmap.mmap, start: int, end: int) -> dict[bytes, list[int]]:
+    station_measures: dict[bytes, list[int]] = {}
     while start < end:
-        name_end = start
-        name_end = mmap_obj.find(b";", name_end)
+        name_end = mmap_obj.find(b";", start)
         measure_end = mmap_obj.find(b"\n", name_end)
-        name = mmap_obj[start: name_end]
-        decimal_pos = mmap_obj.find(b".", name_end)
-        decimal_val = mmap_obj[decimal_pos + 1] - ord(b"0")
-        val = int(mmap_obj[name_end + 1: decimal_pos]) * 10
-        val = val + decimal_val if val >= 0 else val - decimal_val
-        record = Record(val)
-        if name in station_measures:
-            station_measures[name] += record
+        name = mmap_obj[start: mmap_obj.find(b";", start)]
+        val = int(float(mmap_obj[name_end + 1: measure_end]) * 10)
+        record2 = [val, val, val, 1]
+        record1 = station_measures.get(name)
+        if record1:
+            combine_record(record1, record2)
         else:
-            station_measures[name] = record
+            station_measures[name] = record2
         start = measure_end + 1
     return station_measures
 
 
-def combine_res(acc: dict[bytes, Record], curr: dict[bytes, Record]):
-    for name, record in curr.items():
-        if name in acc:
-            acc[name] += record
-        else:
-            acc[name] = record
+def combine_res(acc: dict[bytes, list[int]], curr: dict[bytes, list[int]]):
+    for name, record2 in curr.items():
+        try:
+            combine_record(acc[name], record2)
+        except KeyError:
+            acc[name] = record2
 
 def get_start_and_end_pos(mmap_obj: mmap.mmap) -> list[list[int]]:
     file_size = mmap_obj.size()
     chunk_size = file_size // num_threads
     start_end_pos = [[0, 0]]
-    for i in range(num_threads - 1):
-        s = chunk_size * (i + 1)
-        s = mmap_obj.find(b"\n", s)
+    for i in range(1, num_threads):
+        s = mmap_obj.find(b"\n", chunk_size * i)
         start_end_pos[-1][1] = s
-        start_end_pos.append([s + 1, s + 1])
+        start_end_pos.append([s + 1, 0])
     start_end_pos[-1][-1] = file_size - 1
     return start_end_pos
 
